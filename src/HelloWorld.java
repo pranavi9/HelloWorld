09b084eb815f5de0d7ebf6fcd3f0e9119273c859
pranavi9 <pranavijune17@gmail.com>
2017-03-22 10:18:26 -0400

    commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,8 @@
 
+package oata;
+
 public class HelloWorld {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Hello World!");
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
